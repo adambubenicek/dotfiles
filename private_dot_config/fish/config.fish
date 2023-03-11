63c74ec865a976b73b76087fbb26273b34c8eb47
Adam Bubenicek <adam@adambubenicek.com>
2023-03-11 15:55:17 +0100

    Add better editor handling

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,9 +1,17 @@
 if status is-interactive
+    set -g fish_greeting
     fish_vi_key_bindings
     starship init fish | source
-    
-    set -g fish_greeting
-    set -gx EDITOR hx
-end
 
+    # Helix's binary on Arch is 'helix'
+    if type -q helix
+        set -gx EDITOR helix
+        alias hx="helix"
+    else
+        set -gx EDITOR hx
+    end
+
+    set -gx SUDO_EDITOR $EDITOR
+    set -gx VISUAL $EDITOR
+end
 
