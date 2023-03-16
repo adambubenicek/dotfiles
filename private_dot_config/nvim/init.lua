3a8d79daa8acdf3505f1eb8ea04bf4c7b7631b78
Adam Bubenicek <adam@adambubenicek.com>
2023-03-16 18:13:30 +0100

    Remove unused dependency

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -63,7 +63,6 @@ require("lazy").setup({
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
     },
