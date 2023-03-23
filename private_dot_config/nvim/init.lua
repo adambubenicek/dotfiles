72e02dd1e7bbf4cb67a43e70d01e1351a9876e9c
Adam Bubenicek <adam@adambubenicek.com>
2023-03-23 08:21:29 +0100

    Get better previews

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -124,6 +124,12 @@ require("lazy").setup({
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+      require("telescope").setup({
+        defaults = {
+            layout_strategy = "vertical"
+        }
+      })
     end
   },
   {
