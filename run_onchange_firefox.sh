e4099c60ac1e2918d142cf1799e5a42874257618
Adam Bubenicek <adam@adambubenicek.com>
2023-11-27 18:27:29 +0100

    Open extension links

--- run_onchange_firefox.sh
+++ run_onchange_firefox.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+firefox https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager
+firefox https://addons.mozilla.org/en-US/firefox/addon/linkhints
+firefox https://addons.mozilla.org/en-US/firefox/addon/sponsorblock
+firefox https://addons.mozilla.org/en-US/firefox/addon/ublock-origin
+
 find ~/.mozilla/firefox -regex '.*\.default\(-release\)?' | while read dir; do
 	cat <<EOF > $dir/user.js
 		user_pref("signon.rememberSignons", false);
