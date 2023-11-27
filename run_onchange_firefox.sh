eb8387c6e57f3a895283f8240c9df4f2f13cda72
Adam Bubenicek <adam@adambubenicek.com>
2023-11-27 16:37:18 +0100

    Split configuration scripts

---
+++ run_onchange_firefox.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+find ~/.mozilla/firefox -regex '.*\.default\(-release\)?' | while read dir; do
+	cat <<EOF > $dir/user.js
+		user_pref("signon.rememberSignons", false);
+		user_pref("middlemouse.paste", false);
+		user_pref("browser.toolbars.bookmarks.visibility", "never");
+		user_pref("browser.tabs.firefox-view", false);
+EOF
+done
+
