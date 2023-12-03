e0ddc2c06ca31e070ea58f7e3fba348e0073380c
Adam Bubenicek <adam@adambubenicek.com>
2023-12-03 21:32:13 +0100

    Update keyd layout

--- run_onchange_keyd.sh
+++ run_onchange_keyd.sh
@@ -9,22 +9,21 @@ sudo tee /etc/keyd/default.conf <<EOF
 capslock = esc
 space = overloadt2(nav, space, 200)
 
-a = overloadt2(meta, a, 200)
-s = overloadt2(alt, s, 200)
-d = overloadt2(control, d, 200)
-f = overloadt2(shift, f, 200)
-
-j = overloadt2(shift, j, 200)
-k = overloadt2(control, k, 200)
-l = overloadt2(alt, l, 200)
-; = overloadt2(meta, ;, 200)
-
 [nav]
 h = left
 j = down
 k = up
 l = right
+
+n = home
+m = pagedown
+, = pageup
+. = end
+
+a = oneshot(meta)
+s = oneshot(alt)
+d = oneshot(control)
+f = oneshot(shift)
 EOF
 
 sudo systemctl enable --now keyd
-
